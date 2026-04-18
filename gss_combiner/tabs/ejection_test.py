8185d2969f00d708820f1cbc5abf9e9f9d2c58fa
Jaymil Parikh <jaymilparikh@gmail.com>
2026-04-18 17:11:46 -0500

    ALI

--- gss_combiner/tabs/ejection_test.py
+++ gss_combiner/tabs/ejection_test.py
@@ -16,4 +16,23 @@ import sys
 import queue
 
 def _build_ejection_test_tab(self, parent, name):
-    ttk.Label(parent, text=f"{name} Temporary", font=("Helvetica", 14)).pack(expand=True)
+    ttk.Label(parent,font=("Helvetica", 14)).pack(expand=True)
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    img_path = os.path.join(BASE_DIR,"ali.png")
+    img = tk.PhotoImage(file=img_path)
+    label = ttk.Label(parent, image=img)
+    label.image = img
+    label.place(x=10, y=10)    
+
+    warning = ttk.Label(parent, text="WARNING: Ensure area is clear before initiating pyro test")
+    
+    line = ttk.Label(parent, text="--------------------------------------------------------------------------------------------------------------")
+    line.place(x = 0, y = 120)
+    warning.place(x = 20, y = 10)
+    force_safe_button = ttk.Button(parent, text="Force Safe", padding=(20, 20), command=lambda: print("Force Safe button clicked!"))
+    force_safe_button.place(x=20,y=250)
+
+    pyro_test_button = ttk.Button(parent, text="Pyro Test", padding=(20, 20), command=lambda: print("Pyro Test button clicked!"))
+    pyro_test_button.place(x=150,y=250)
+
