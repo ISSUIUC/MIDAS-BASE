8c040dbe643181056e8d552add24058b7e2b6b71
Jaymil Parikh <jaymilparikh@gmail.com>
2026-04-21 19:07:28 -0500

    force safe and pyro test buttons work

--- gss_combiner/tabs/ejection_test.py
+++ gss_combiner/tabs/ejection_test.py
@@ -27,31 +27,39 @@ def _build_ejection_test_tab(self, parent, name):
 
     warning = ttk.Label(parent, text="WARNING: Ensure area is clear before initiating pyro test")
     
-    line = ttk.Label(parent, text="--------------------------------------------------------------------------------------------------------------")
-    line.place(x = 0, y = 120)
+    # line = ttk.Label(parent, text="--------------------------------------------------------------------------------------------------------------")
+    # line.place(x = 0, y = 120)
     warning.place(x = 20, y = 10)
-    force_safe_button = ttk.Button(parent, text="Force Safe", padding=(20, 20), command=force_safe)
+    force_safe_button = ttk.Button(parent, text="Force Safe", padding=(20, 20), command=lambda: force_safe(self))
     force_safe_button.place(x=20,y=250)
 
-    pyro_test_button = ttk.Button(parent, text="Pyro Test", padding=(20, 20), command=pyro_test)
+    pyro_test_button = ttk.Button(parent, text="Pyro Test", padding=(20, 20), command=lambda: pyro_test(self))
     pyro_test_button.place(x=150,y=250)
 
-    fire_A_button = ttk.Button(parent, text="Fire A", padding=(20, 20), command=fire_A)
-    fire_A_button.place(x=310,y=250)
+    self.fire_A_button = ttk.Button(parent, text="Fire A", state="disabled", padding=(20, 20), command=fire_A)
+    self.fire_A_button.place(x=310,y=250)
 
-    fire_B_button = ttk.Button(parent, text="Fire B", padding=(20, 20), command=fire_B)
-    fire_B_button.place(x=430,y=250)
+    self.fire_B_button = ttk.Button(parent, text="Fire B", state="disabled", padding=(20, 20), command=fire_B)
+    self.fire_B_button.place(x=430,y=250)
     
-    fire_C_button = ttk.Button(parent, text="Fire C", padding=(20, 20), command=fire_C)
-    fire_C_button.place(x=550,y=250)
+    self.fire_C_button = ttk.Button(parent, text="Fire C", state="disabled", padding=(20, 20), command=fire_C)
+    self.fire_C_button.place(x=550,y=250)
 
-    fire_D_button = ttk.Button(parent, text="Fire D", padding=(20, 20), command=fire_D)
-    fire_D_button.place(x=670,y=250)
+    self.fire_D_button = ttk.Button(parent, text="Fire D", state="disabled", padding=(20, 20), command=fire_D)
+    self.fire_D_button.place(x=670,y=250)
 
-def force_safe():
+def force_safe(self):
+    self.fire_A_button.config(state="disabled")
+    self.fire_B_button.config(state="disabled")
+    self.fire_C_button.config(state="disabled")
+    self.fire_D_button.config(state="disabled")
     print("force safe")
 
-def pyro_test():
+def pyro_test(self):
+    self.fire_A_button.config(state="normal")
+    self.fire_B_button.config(state="normal")
+    self.fire_C_button.config(state="normal")
+    self.fire_D_button.config(state="normal")
     print("pyro test")
 
 def fire_A():
